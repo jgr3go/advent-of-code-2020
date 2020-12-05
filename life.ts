f9d81ce760c976c4e220b7394707f933248d066d
Jon Gregorowicz <jonathan.gregorowicz@gmail.com>
2020-12-05 13:38:29 -0500

    build Conway's Game of Life emulator

--- life.ts
+++ life.ts
@@ -4,7 +4,6 @@ import * as _ from 'lodash';
 
 class Manager {
   grid: Grid;
-  iterationMS: number = 500;
   iterations: number = 0;
 
   constructor(width: number, height: number, iterationMS: number = 500) {
@@ -13,24 +12,24 @@ class Manager {
   
   print() {
     process.stdout.cursorTo(0, 0);
-    process.stdout.write(`Iteration ${this.iterations}`)
+    process.stdout.write(`Iteration ${this.iterations}   `)
     for (let row = 0; row < this.grid.cells.length; row++) {
       for (let col = 0; col < this.grid.cells[0].length; col++) {
         let cell = this.grid.cells[row][col];
         process.stdout.cursorTo(col, row+1);
-        process.stdout.clearLine(0);
-        process.stdout.write(cell.alive ? '▉' : ' ');
+        // process.stdout.clearLine(0);
+        process.stdout.write(cell.alive ? 'X' : ' ');
       }
     }
   }
 
-  iterate() {
+  iterate(ms: number = 1000) {
     setTimeout(() => {
       this.iterations++;
       this.grid.iterate();
       this.print();
       this.iterate();
-    }, this.iterationMS);
+    }, ms);
   }
 }
 
@@ -44,32 +43,16 @@ class Grid {
     this.width = width;
     this.height = height;
     this.cells = [];
-    for (let row = 0; row < height; row++) {
+    for (let row = 0; row < this.height; row++) {
       let curRow: Cell[] = [];
-      for (let col = 0; col < width; col++) {
+      for (let col = 0; col < this.width; col++) {
         curRow.push(new Cell(row, col, false));
       }
       this.cells.push(curRow);
     }
   }
 
-  shouldLive(cell: Cell) {
-    let neighbors = this.getNeighbors(cell);
-    return neighbors.reduce((count, neighbor) => count + (neighbor.alive ? 1 : 0), 0) > 0;
-  }
-
-  getNeighbors(cell: Cell) {
-    let neighbors: Cell[] = [];
-    let keys = [{x:-1,y:-1},{x:-1,y:0},{x:-1,y:1},{x:0,y:-1},{x:0,y:1},{x:1,y:-1},{x:1,y:0},{x:1,y:1}];
-    for (let key of keys) {
-      if (this.cells[cell.x+key.x] && this.cells[cell.x+key.x][cell.y+key.y]) {
-        neighbors.push(this.cells[cell.x+key.x][cell.y+key.y]);
-      }
-    }
-    return neighbors;
-  }
-
-  iterate() {
+  rebuild() {
     let newCells: Cell[][] = [];
     for (let row = 0; row < this.height; row++) {
       let curRow: Cell[] = [];
@@ -82,23 +65,112 @@ class Grid {
     }
     this.cells = newCells;
   }
+
+  shouldLive(cell: Cell) {
+    let neighbors = this.getNeighbors(cell);
+    let alive = neighbors.reduce((count, neighbor) => count + (neighbor.alive ? 1 : 0), 0);
+    if (cell.alive && (2 <= alive && alive <= 3)) return true;
+    if (!cell.alive && alive == 3) return true;
+    return false;
+  }
+
+  getNeighbors(cell: Cell) {
+    let neighbors: Cell[] = [];
+    let deltas = [
+      {row:-1,col:-1},
+      {row:-1,col:0},
+      {row:-1,col:1},
+      {row:0,col:-1},
+      {row:0,col:1},
+      {row:1,col:-1},
+      {row:1,col:0},
+      {row:1,col:1}
+    ];
+    for (let delta of deltas) {
+      let rIdx = cell.row + delta.row;
+      let cIdx = cell.col + delta.col;
+      if (this.cells[rIdx] && this.cells[rIdx][cIdx]) {
+        neighbors.push(this.cells[rIdx][cIdx]);
+      }
+    }
+    return neighbors;
+  }
+
+  iterate() {
+    this.rebuild();
+  }
 }
 
 class Cell {
-  x: number;
-  y: number;
+  row: number;
+  col: number;
   alive: boolean = false;
 
-  constructor(x: number, y: number, alive: boolean) {
-    this.x = x;
-    this.y = y;
+  constructor(row: number, col: number, alive: boolean) {
+    this.row = row;
+    this.col = col;
     this.alive = alive;
   }
 }
 
-let manager = new Manager(5, 5);
-manager.grid.cells[1][2].alive = true;
-manager.grid.cells[2][2].alive = true;
-manager.grid.cells[3][2].alive = true;
+let Oscillators = {
+  createBlinker: (manager: Manager, row: number, col: number) => {
+    manager.grid.cells[row][col].alive = true;
+    manager.grid.cells[row][col+1].alive = true;
+    manager.grid.cells[row][col+2].alive = true;
+  },
+  createToad: (manager: Manager, row: number, col: number) => {
+    manager.grid.cells[row][col].alive = true;
+    manager.grid.cells[row][col+1].alive = true;
+    manager.grid.cells[row][col+2].alive = true;
+    manager.grid.cells[row-1][col+1].alive = true;
+    manager.grid.cells[row-1][col+2].alive = true;
+    manager.grid.cells[row-1][col+3].alive = true;
+  },
+  createBeacon: (manager: Manager, row: number, col: number) => {
+    manager.grid.cells[row][col].alive = true;
+    manager.grid.cells[row][col+1].alive = true;
+    manager.grid.cells[row+1][col].alive = true;
+    manager.grid.cells[row+1][col+1].alive = true;
+    manager.grid.cells[row+2][col+2].alive = true;
+    manager.grid.cells[row+2][col+3].alive = true;
+    manager.grid.cells[row+3][col+2].alive = true;
+    manager.grid.cells[row+3][col+3].alive = true;
+  }
+  
+}
 
-manager.iterate();
+let Spaceships = {
+  createGlider: (manager: Manager, row: number, col: number) => {
+    manager.grid.cells[row][col].alive = true;
+    manager.grid.cells[row+1][col+1].alive = true;
+    manager.grid.cells[row+1][col+2].alive = true;
+    manager.grid.cells[row][col+2].alive = true;
+    manager.grid.cells[row-1][col+2].alive = true;
+  }
+}
+
+
+
+
+let width = 100;
+let height = 20;
+let living = 300;
+
+let manager = new Manager(width, height);
+Oscillators.createBlinker(manager, 3, 2);
+Oscillators.createToad(manager, 10, 2);
+Oscillators.createBeacon(manager, 3, 8);
+Spaceships.createGlider(manager, 3, 14);
+
+for (let ii = 0; ii < living; ii++) {
+  let randRow = Math.floor(Math.random() * height);
+  let randCol = Math.floor(Math.random() * width);
+  manager.grid.cells[randRow][randCol].alive = true;
+}
+
+
+
+manager.iterate(500);
+
+
